cb9cbc38c44848c6b186f3c8224fb6e480b9d09e
yliaho <joonas.yliaho@gmail.com>
2019-07-29 15:01:05 +0300

    try/catch on index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import 'module-alias/register'
 import { JackuBot } from './jackubot'
 
-new JackuBot({
-  gitlab: {
-    allowPrivateRepositories: true,
-    privates: {
-      privateToken: process.env.GITLAB_PRIVATE_TOKEN
+try {
+  new JackuBot({
+    gitlab: {
+      allowPrivateRepositories: true,
+      privates: {
+        privateToken: process.env.GITLAB_PRIVATE_TOKEN
+      }
+    },
+    discord: {
+      webhookUrl: process.env.DISCORD_WEBHOOK_URL
     }
-  },
-  discord: {
-    webhookUrl: process.env.DISCORD_WEBHOOK_URL
-  }
-})
+  }).start()
+} catch (err) {
+  console.error(err)
+}
